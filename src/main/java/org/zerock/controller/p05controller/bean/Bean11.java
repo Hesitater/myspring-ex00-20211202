8be85be952bfac22f338046b3de5fd63c0daf514
Hesitater <feelksh0603@gmail.com>
2021-12-08 20:04:35 +0900

    alias

---
+++ src/main/java/org/zerock/controller/p05controller/bean/Bean11.java
@@ -0,0 +1,10 @@
+package org.zerock.controller.p05controller.bean;
+
+import lombok.Data;
+
+@Data
+public class Bean11 {
+//	private Integer customerID;
+	private Integer id;
+	private String customerName;
+}
