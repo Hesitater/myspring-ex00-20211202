3dcbf84c2e99576f7dad100f731e2b7613781329
Hesitater <feelksh0603@gmail.com>
2021-12-08 20:04:48 +0900

    getcustomer

---
+++ src/main/java/org/zerock/controller/p05controller/bean/Bean14.java
@@ -0,0 +1,17 @@
+package org.zerock.controller.p05controller.bean;
+
+import lombok.Data;
+
+@Data
+public class Bean14 {
+	
+	private Integer customerID;
+	private String customerName;
+	private String contactName;
+	private String address;
+	private String city;
+	private String PostalCode;
+	private String Country;
+	
+	
+}
