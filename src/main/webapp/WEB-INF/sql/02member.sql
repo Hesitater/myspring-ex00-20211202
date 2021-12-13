245a1f07fcb7d67bd5374a48a81531a744bbd33e
Hesitater <feelksh0603@gmail.com>
2021-12-14 08:49:55 +0900

    sql, insert test

---
+++ src/main/webapp/WEB-INF/sql/02member.sql
@@ -0,0 +1,29 @@
+USE test;
+
+CREATE TABLE Member (
+	id VARCHAR(30) PRIMARY KEY,
+    password VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL
+);
+
+ALTER TABLE Member ADD COLUMN inserted DATETIME NOT NULL DEFAULT NOW();
+
+SELECT * FROM Member ORDER BY inserted DESC;
+SELECT * FROM Member;
+DESC Member;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
