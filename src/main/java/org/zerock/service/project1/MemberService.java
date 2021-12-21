1b9925a0128fb8c1ae8a620989e7ad9bec5e58e3
Hesitater <feelksh0603@gmail.com>
2021-12-21 12:31:02 +0900

    service, mapper

--- src/main/java/org/zerock/service/project1/MemberService.java
+++ src/main/java/org/zerock/service/project1/MemberService.java
@@ -5,13 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.zerock.domain.project1.MemberVO;
+import org.zerock.domain.project1.PageInfoVO;
 import org.zerock.mapper.project1.MemberMapper;
 
 import lombok.Setter;
 
 @Service
 public class MemberService {
-	
+
 	@Setter(onMethod_ = @Autowired)
 	private MemberMapper mapper;
 	
@@ -26,17 +27,12 @@ public class MemberService {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
 		return false;
 	}
 	
 	public boolean modify(MemberVO member) {
-	
-		try {
-			return mapper.update(member) == 1;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return false;
+		return mapper.update(member) == 1;
 	}
 	
 	public boolean remove(String id) {
@@ -46,14 +42,58 @@ public class MemberService {
 	public List<MemberVO> getList() {
 		return mapper.list();
 	}
+
+	public List<MemberVO> getList(Integer page, Integer numberPerPage) {
+		Integer from = (page - 1) * numberPerPage;
+		
+		return mapper.listPage(from, numberPerPage);
+	}
+
+	public PageInfoVO getPageInfo(Integer page, Integer numberPerPage) {
+		Integer countRows = mapper.getCountRows(); // 총 레코드 수 
+		Integer lastPage = (countRows - 1) / numberPerPage + 1; // 마지막 페이지 번호
+		
+		Integer leftPageNumber = page - 5; // 가장 왼쪽 페이지 번호
+		leftPageNumber = Math.max(1, leftPageNumber);
+		
+		Integer rightPageNumber = leftPageNumber + 9; // 가장 오른쪽 페이지 번호
+		rightPageNumber = Math.min(rightPageNumber, lastPage);
+		
+		Boolean hasNextButton = page != lastPage;
+		Boolean hasPrevButton = page != 1;
+		
+
+		PageInfoVO pageInfo = new PageInfoVO();
+		
+		pageInfo.setLastPage(lastPage);
+		pageInfo.setCurrentPage(page);
+		pageInfo.setCountRows(countRows);
+		pageInfo.setLeftPageNumber(leftPageNumber);
+		pageInfo.setRightPageNumber(rightPageNumber);
+		pageInfo.setHasNextButton(hasNextButton);
+		pageInfo.setHasPrevButton(hasPrevButton);
+		
+		
+		return pageInfo;
+	}
+
+	public boolean hasId(String id) {
+		
+		MemberVO member = mapper.select(id);
+		
+		return member != null;
+	}
+
+	public boolean hasNickName(String nickName) {
+		
+		MemberVO memberNickName = mapper.selectNickName(nickName);
+		
+		return memberNickName != null;
+	}
+
+
 	
 }
-
-
-
-
-
-
 
 
 
