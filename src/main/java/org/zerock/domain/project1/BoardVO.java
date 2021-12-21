6c58d43b36a5a9980065d8f47d70cb9a899c3f44
Hesitater <feelksh0603@gmail.com>
2021-12-21 12:30:17 +0900

    restcontroller

--- src/main/java/org/zerock/domain/project1/BoardVO.java
+++ src/main/java/org/zerock/domain/project1/BoardVO.java
@@ -1,6 +1,10 @@
 package org.zerock.domain.project1;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 
 import lombok.Data;
 
@@ -11,8 +15,36 @@ public class BoardVO {
 	private String title;
 	private String content;
 	private String writer;
+//	@JsonFormat(shape = Shape.STRING)
 	private LocalDateTime inserted;
+//	@JsonFormat(shape = Shape.STRING)
 	private LocalDateTime updated;
 	private String nickName;
 	
+	
+	
+	public String getCustomInserted() {
+		//현재 일시
+		LocalDateTime now = LocalDateTime.now(ZoneId.of("+09:00"));
+		
+		LocalDateTime beforeOneDayFromNow = now.minusDays(1);
+		
+		if(inserted.isBefore(beforeOneDayFromNow)) {
+			return inserted.toLocalDate().toString();
+		} else {
+			return inserted.toLocalTime().toString();
+		}
+		
+	}
+	
+	
 }
+
+
+
+
+
+
+
+
+
